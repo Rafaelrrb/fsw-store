28bbcbf39161e6214dd450e848d9591c9e01d91a
Rafaelrrb <rafaeborba@gmail.com>
2023-10-19 02:47:49 -0300

    feat: add Footer components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image"
 import Categories from "./components/categories"
 import { prismaClient } from "@/lib/prisma"
 import ProductList from "./components/productList"
 import SectionTitle from "./components/sectionTitle"
 import PromoBanner from "./components/promoBanner"
+
 
 
 
@@ -24,25 +24,40 @@ export default async function Home() {
     }
   })
 
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: 'mouses'
+      }
+    }
+  })
+
   return (
-   <div className="">
+   <div className="flex flex-col gap-8 py-8">
     
     <PromoBanner src="/banner-home-01.png" alt="Até 50% desconto esse mês!"/>
 
-    <div className="mt-8 px-5">
+    <div className=" px-5">
       <Categories />
     </div>
 
-    <div className="mt-8">
+    <div >
       <SectionTitle>Ofertas</SectionTitle>
       <ProductList products={deals}/>
     </div>
     
     <PromoBanner src="/banner-home-02.png" alt="Até 55% de desconto em mouses!"/>
 
-    <div className="mt-8">
+    <div >
       <SectionTitle>Teclado</SectionTitle>
       <ProductList products={keyboards}/>
+    </div>
+
+    <PromoBanner src="/banner-home-03.png" alt="Até 20% de desconto em Fones!"/>
+
+    <div >
+      <SectionTitle>Mouses</SectionTitle>
+      <ProductList products={mouses}/>
     </div>
 
    </div>
