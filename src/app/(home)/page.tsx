6bc38671fe34809ea1d8032809e8f9214edb1906
Rafaelrrb <rafaeborba@gmail.com>
2023-10-18 23:55:49 -0300

    feat: add second banner  to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -24,14 +24,25 @@ export default async function Home() {
       sizes="100vw"
       alt="Até 50% desconto esse mês!"
     />
+
     <div className="mt-8 px-5">
       <Categories />
-    
-    <div className="mt-8">
-      <ProductList products={deals}/>
     </div>
 
+    <div className="mt-8">
+      <p className="font-bold uppercase pl-5 mb-5">Ofertas</p>
+      <ProductList products={deals}/>
     </div>
+    
+    <Image 
+      src="/banner-home-02.png"
+      height={0}
+      width={0}
+      className="h-auto w-full px-5 mt-5"
+      sizes="100vw"
+      alt="Até 55% de desconto em mouses!"
+    />
+    
    </div>
   )
 }
