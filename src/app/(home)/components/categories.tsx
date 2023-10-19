77754d4e6616b5ef6e9a6593c099706449debacb
Rafaelrrb <rafaeborba@gmail.com>
2023-10-18 23:37:04 -0300

    feat: add ProductItem and ProductList components

--- src/app/(home)/components/categories.tsx
+++ src/app/(home)/components/categories.tsx
@@ -5,8 +5,8 @@ const Categories = async ()=>{
   const categories = await prismaClient.category.findMany({})
   return(
     <div className="grid grid-cols-2 gap-x-4 gap-y-2">
-      {categories.map((category)=>
-      (<CategoryItem key={category.id} category={category}/>))}
+      {categories.map(category=>
+      <CategoryItem key={category.id} category={category}/>)}
     </div>
   );
 }
